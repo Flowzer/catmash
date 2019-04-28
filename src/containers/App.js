b8e9baf04eb9e90dd1be185a97b296c192fa6f9c
Florient <florient.sok@gmail.com>
2019-04-28 21:40:02 +0200

    Affichage d'images aléatoire lors d'un clique

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,57 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import CatCompare from './cat-compare';
+
+
+let RANDOM_NUMBER1, RANDOM_NUMBER2 = 0;
 
 class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {catList:{},catRandom:{}};
   }
 
   componentWillMount(){
+    this.getRandomNumber();
     this.getData()
+}
+onClickListItem(cat){
+  console.log(cat)
+  this.componentWillMount();
+}
+
+getRandomNumber(){
+  RANDOM_NUMBER1 = Math.floor(Math.random() * 100);
+  RANDOM_NUMBER2 = Math.floor(Math.random() * 100);
 }
 
 //Permet de récupérer les données de l'URL
-  getData(){
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://latelier.co/data/cats.json";
-    fetch(proxyurl + url)
-    .then(response => response.text())
-    .then(contents => console.log(JSON.parse(contents)))
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
+getData(){
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = "https://latelier.co/data/cats.json";
+
+  fetch(proxyurl + url).then(response => response.text())
+    .then(contents => JSON.parse(contents))
+      .then(data => this.setState({catList:data.images,catRandom:[data.images[RANDOM_NUMBER1],data.images[RANDOM_NUMBER2]]}))
+        .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
     
 }
   render(){
+    const renderCatCompare = () => {
+      if (this.state.catRandom.length > 0){
+          return <CatCompare catRandom={this.state.catRandom} callback={this.onClickListItem.bind(this)}/>
+      }
+  }
     return (
       <div className="App">
         <header className="App-header">
           <h1>CATMASH</h1>
+          <div>
+          {renderCatCompare()}
+          </div> 
         </header>
       </div>
+       
     );
   }
   
